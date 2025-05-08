5867ed0b2fec66fafe6ee16e85363be71f4cfb60
Konstantin Kolesnikov <kolesnikov.kons@gmail.com>
2025-05-08 18:03:21 +0500

    Поменял урлы

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const BACKEND = 'http://localhost:8000';
-const WS_BACKEND = 'ws://localhost:8000/ws/game/';
+const BACKEND = 'https://api.guessthemelody.ru';
+const WS_BACKEND = 'wss://api.guessthemelody.ru/ws/game/';
 
 export { BACKEND, WS_BACKEND };
