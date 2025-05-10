498bb73962973887dae6d508df0cdf7277a3d342
Konstantin Kolesnikov <kolesnikov.kons@gmail.com>
2025-05-10 20:03:06 +0500

    Update config

--- js/config.js
+++ js/config.js
@@ -1,4 +1,7 @@
-const BACKEND = 'http://localhost:8000';
-const WS_BACKEND = 'ws://localhost:8000/ws/game/';
+//const BACKEND = 'http://localhost:8000';
+//const WS_BACKEND = 'ws://localhost:8000/ws/game/';
+
+const BACKEND = 'https://api.guessthemelody.ru';
+const WS_BACKEND = 'wss://https://api.guessthemelody.ru/ws/game/';
 
 export { BACKEND, WS_BACKEND };
