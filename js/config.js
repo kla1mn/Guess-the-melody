b5dbf2b8ffef26ea68cc3ab889c2f9930b6b49be
S4ske <sany1111.ru@mail.ru>
2025-06-23 11:48:59 +0500

    Убрал лишнее к зачёту

--- js/config.js
+++ js/config.js
@@ -1,5 +1,2 @@
-//export const BACKEND = 'http://localhost:8000';
-//export const WS_BACKEND = 'ws://localhost:8000/ws/game/';
-
 export const BACKEND = 'https://api.guessthemelody.ru';
 export const WS_BACKEND = 'wss://api.guessthemelody.ru/ws/game/';
